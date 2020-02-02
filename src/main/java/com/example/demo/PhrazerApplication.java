70564c66ee96af17e97ec81c1185accdd2ebdc9f
Martynas <martynas.gudai@gmail.com>
2020-02-02 17:43:10 +0100

    Spring Boot files

---
+++ src/main/java/com/example/demo/PhrazerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhrazerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhrazerApplication.class, args);
+	}
+
+}
